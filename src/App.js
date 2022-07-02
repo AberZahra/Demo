bc6f4c76116ec24fadbfc6c99a1d99eec7ea9213
Zahra <azhayat3@gmail.com>
2022-07-02 16:57:41 +0500

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from './components/demo/Demo';
+
+function App() {
+  return (
+    <>
+      <Demo />
+    </>
+  );
+}
+
+export default App;
